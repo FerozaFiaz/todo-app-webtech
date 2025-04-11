9b6e7ad2d68d8f9f5eaf6e95eaa95a3459284c33
FerozaFiaz <ferozafiaz@gmail.com>
2025-04-11 03:53:32 +0200

    Commit 1 - M1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # todo-app-webtech
+# Feroza Fiaz (s0594048) und Fiza Fiaz (s0596772 )
---
+++ src/main/java/controller/TaskController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import de.htwberlin.webtech.webtech.Task;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+@CrossOrigin(origins = "*") // erlaubt Zugriff vom Frontend (Vue.js)
+public class TaskController {
+
+    @GetMapping
+    public List<Task> getDummyTasks() {
+        Task task1 = new Task("Webtechnologie lernen", "M1 bis 20. April erledigen");
+        Task task2 = new Task("Vue.js vorbereiten", "Für M2 ein v-for schreiben");
+
+        return List.of(task1, task2);
+    }
+}
